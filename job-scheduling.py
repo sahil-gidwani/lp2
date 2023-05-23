d921521d6769195c6691f1356901f333512b8928
Sahil Gidwani <79597840+sahil-gidwani@users.noreply.github.com>
2023-05-23 20:37:54 +0530

    Create job-scheduling.py

---
+++ job-scheduling.py
@@ -0,0 +1,39 @@
+def printJobScheduling(arr, t):
+	n = len(arr)
+
+	# sort all jobs according to decreasing order of profit
+	for i in range(n):
+		for j in range(n - 1 - i):
+			if arr[j][2] < arr[j + 1][2]:
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+	# to keep track of free time slots
+	result = [False] * t
+
+	# to store result (sequence of jobs)
+	job = ['-1'] * t
+
+	# iterate through all given jobs
+	for i in range(len(arr)):
+
+		# find a free slot for this job
+		# (note that we start from the last possible slot)
+		for j in range(min(t - 1, arr[i][1] - 1), -1, -1):
+
+			# free slot found
+			if result[j] is False:
+				result[j] = True
+				job[j] = arr[i][0]
+				break
+
+	print(job)
+
+arr = [['a', 2, 100],
+        ['b', 1, 19],
+        ['c', 2, 27],
+        ['d', 1, 25],
+        ['e', 3, 15]]
+
+
+print("Following is maximum profit sequence of jobs")
+printJobScheduling(arr, 3)
