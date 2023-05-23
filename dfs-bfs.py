6adb80d9d1708ae834a15a2d192bd6bbd7daf144
Sahil Gidwani <79597840+sahil-gidwani@users.noreply.github.com>
2023-05-23 19:17:38 +0530

    Create dfs-bfs.py

---
+++ dfs-bfs.py
@@ -0,0 +1,83 @@
+from collections import defaultdict
+
+
+class Graph:
+
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def displayGraph(self):
+        for u in self.graph:
+            print(u, " -> ", end="")
+            for v in self.graph[u]:
+                print(v, end=" ")
+            print()
+
+    def DFSUtil(self, v, visited):
+        visited[v] = True
+
+        print(v)
+
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.DFSUtil(i, visited)
+
+    def DFS(self):
+        V = len(self.graph)
+        visited = [False]*(V)
+
+        for i in range(V):
+            if visited[i] == False:
+                self.DFSUtil(i, visited)
+        
+        # self.DFSUtil(2, visited) # to start from a particular vertex
+
+    def BFS(self):
+        s = 0
+
+        V = len(self.graph)
+        visited = [False]*(V)
+
+        queue = []
+
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            v = queue.pop(0)
+            print(v)
+
+            for i in self.graph[v]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+# Take input for adding edges
+# while True:
+#     u = input("Enter the source vertex (or 'q' to quit): ")
+#     if u == 'q':
+#         break
+#     v = input("Enter the destination vertex: ")
+#     g.addEdge(int(u), int(v))
+
+# Display the graph
+print("Graph:")
+g.displayGraph()
+
+print("Following is Depth First Traversal")
+g.DFS()
+
+print("Following is Breadth First Traversal (starting from vertex 0)")
+g.BFS()
